fefd9e6bf7afd23b525b6b3417ee6644213023ff
gavin1818 <gavin1818@hotmail.com>
2017-05-30 23:03:51 -0700

    some chagne

--- Calculation.java
+++ Calculation.java
@@ -1,9 +1,8 @@
 import java.util.*;
+import java.text.*;
 public class Calculation{
 	private static Calculation instance = null;
-   	private Calculation() {
-		
-   	}
+   	private Calculation() {}
 	public static Calculation getInstance() {
 	  if(instance == null) {
 	     instance = new Calculation();
@@ -11,18 +10,21 @@ public class Calculation{
 	  return instance;
 	}
 
-	public void calculate(int c, int n, List<Double> testArray){
+	public Float[][] calculate(int c, int n, List<Float> testArray){
+		DecimalFormat df = new DecimalFormat("#.######");
 		Float[][] res = new Float[c+1][c+1]; 
 		for(int k=0;k<=c;k++){
 			for(int j=0;j<=c;j++){
 				float subresult=0; 
 				for(int i=c;i<n;i++){
-					double tempNumber = testArray.get(i-k)*testArray.get(i-j);
-					subresult +=(float)tempNumber;
+					Float tempNumber = testArray.get(i-k)*testArray.get(i-j);
+					subresult +=tempNumber;
 				}
+				subresult = Float.parseFloat(df.format(subresult));
 				res[k][j]=subresult;
 				System.out.println(subresult);
 			}
 		}
-	}	
+		return res;
+	}
 }
--- MasimoMain.java
+++ MasimoMain.java
@@ -1,28 +1,33 @@
-
 import java.util.*;
 import java.io.*;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
-import java.io.IOException;
-import java.util.Scanner;
-import java.util.Queue;
-import java.util.regex.*;
 
 public class MasimoMain{
-	public static void main(String arg[]) throws IOException{
+	public static void main(String arg[]) throws Exception{
         String fileAddress = "TEST.txt";
 		Scanner console = new Scanner( System.in );
         String choice = "s";
-        String c="";
-        String n="";
-        List<Double> testList= readFromText(fileAddress);
+        int c,n;
+        List<Float> testList= readFromText(fileAddress);
 		while(true){
 			if(choice.startsWith("s")){
-				System.out.print("what is the c? ");
-				c = console.nextLine().trim();
-                System.out.print("what is the N? ");
-                n = console.nextLine().trim();               
-                Calculation.getInstance().calculate(Integer.parseInt(c),Integer.parseInt(n),testList);
+                try{
+                    System.out.print("what is the c? ");
+                    c = Integer.valueOf(console.nextLine().trim());
+                    System.out.print("what is the N? ");
+                    n = Integer.valueOf(console.nextLine().trim());
+                    while(c>=n||c<0||n>300)
+                    {
+                        System.out.println("please enter valid c and N. (0<=c<n<=300)");
+                        System.out.print("what is the c? ");
+                        c = Integer.valueOf(console.nextLine().trim());
+                        System.out.print("what is the N? ");
+                        n = Integer.valueOf(console.nextLine().trim());   
+                    }
+                    Calculation.getInstance().calculate(c,n,testList);
+                }catch(IllegalArgumentException iae){
+                    System.out.println("Can not parse Input, please try again? ");
+                }
+				
             }else if ( choice.startsWith( "q" ) ) {
                 break;
             }
@@ -32,18 +37,17 @@ public class MasimoMain{
 
 		}
 	}
-	public static List<Double> readFromText(String address) throws IOException {
-        Scanner inFile = new Scanner(new File(address));
-
-        List<Double> temps = new ArrayList<>();
-        // while loop
-        while (inFile.hasNext()) {
-          // find next line
-          String token = inFile.next();
-          temps.add(Double.parseDouble(token));
-        }
-        inFile.close();
-        return temps;           
+	public static List<Float> readFromText(String address) throws IOException {
+            Scanner inFile = new Scanner(new File(address));
+            List<Float> temps = new ArrayList<>();
+            // while loop
+            while (inFile.hasNext()) {
+              // find next line
+              String token = inFile.next();
+              temps.add(Float.parseFloat(token));
+            }
+            inFile.close();
+            return temps;       
     }
 
 } 
