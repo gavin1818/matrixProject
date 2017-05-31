f16d57439cac273f8a84402a338c7f521129df19
gavin1818 <gavin1818@hotmail.com>
2017-05-30 23:13:55 -0700

    some chagne

--- Calculation.java
+++ Calculation.java
@@ -3,6 +3,7 @@ import java.text.*;
 public class Calculation{
 	private static Calculation instance = null;
    	private Calculation() {}
+   	//use as a singleton
 	public static Calculation getInstance() {
 	  if(instance == null) {
 	     instance = new Calculation();
@@ -10,14 +11,21 @@ public class Calculation{
 	  return instance;
 	}
 
-	public Float[][] calculate(int c, int n, List<Float> testArray){
+	/**
+	* calcuate and return the matrix
+	*@param c
+	*@param n
+	*@param nArray, the Array of N
+	*@return Matix
+	*/
+	public Float[][] calculate(int c, int n, List<Float> nArray){
 		DecimalFormat df = new DecimalFormat("#.######");
 		Float[][] res = new Float[c+1][c+1]; 
 		for(int k=0;k<=c;k++){
 			for(int j=0;j<=c;j++){
 				float subresult=0; 
 				for(int i=c;i<n;i++){
-					Float tempNumber = testArray.get(i-k)*testArray.get(i-j);
+					Float tempNumber = nArray.get(i-k)*nArray.get(i-j);
 					subresult +=tempNumber;
 				}
 				subresult = Float.parseFloat(df.format(subresult));
